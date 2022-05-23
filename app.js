1ad2cf2168e875d841057483fa72c94fee9a18d8
lbaker15 <laelbaker@hotmail.co.uk>
2022-05-23 14:47:08 +0100

    inital

--- app.js
+++ app.js
@@ -3,12 +3,26 @@ const path = require('path');
 const routes = require('./routes/api.js');
 const port = process.env.PORT || 3000;
 const cors = require('cors');
-const bodyParser = require('body-parser');
+const promMid = require('express-prometheus-middleware');
+// const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({extended: false}));
+app.use(function(req, res, next) {
+  if (!req.headers.authorization | req.headers.authorization !== 'mysecrettoken') {
+    return res.status(403).json({ error: 'No credentials sent!' });
+  }
+  next();
+});
+app.use(promMid({
+    metricsPath: '/metrics',
+    collectDefaultMetrics: true,
+    requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+    requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+    responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+}));
 
 app.use('/', routes);
 
--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const one = require('../controllers/one');
 
-router.get('/test', one.test);
+router.get('/time', (req, res, next) => {
+    let timestamp = Date.now() / 1000;
+    console.log(timestamp)
+    let json = {'epoch': Math.round(timestamp)}
+    res.json(json)
+});
 
 module.exports = router;
